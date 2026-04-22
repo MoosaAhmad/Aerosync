80c1dba08889afc47bda49890ff45f7f318a0977
Moosa-Ahmad <moosa.bvtl@gmail.com>
2026-04-23 00:34:31 +0500

    Created separate header files for classes, so everyone can work on separate files at the same time and reduce merge confilcts

--- repo 3/test file.cpp
+++ repo 3/test file.cpp
@@ -1,7 +1,7 @@
-//#include <iostream>
-//
-//int main()
-//{
-//    std::cout << "Hello World!\n";
-//    return 0;
-//}
+#include <iostream>
+// this is just a test file, when you make your code/functions and programs, you can run them here when you dont want to make any changes to the main file.
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}

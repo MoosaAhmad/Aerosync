98f3505295c63e05c9d5db4b51f18dad8b3ff783
OwaisMazhar-11 <owais74lhr@gmail.com>
2026-04-24 19:25:54 +0500

    Update Main.cpp

--- repo 3/Main.cpp
+++ repo 3/Main.cpp
@@ -1,6 +1,45 @@
 #include<iostream>
 using namespace std;
 
+class ProfitCalculator
+{
+private:
+    double totalCost;
+    double totalRevenue;
+    int ticketsSold;
+
+public:
+    ProfitCalculator(double cost = 0, double revenue = 0, int tickets = 0)
+    {
+        totalCost = cost;
+        totalRevenue = revenue;
+        ticketsSold = tickets;
+    }
+
+    // Methods
+
+    double calcProfit()
+    {
+        return totalRevenue - totalCost;
+    }
+    bool isProfitable()
+    {
+        return calcProfit() > 0;
+    }
+    void displayReport()
+    {
+        cout << "Total Cost: " << totalCost << endl;
+        cout << "Total Revenue: " << totalRevenue << endl;
+        cout << "Tickets Sold: " << ticketsSold << endl;
+        cout << "Profit: " << calcProfit() << endl;
+
+        if (isProfitable())
+            cout << "Status: Profitable" << endl;
+        else
+            cout << "Status: Not Profitable" << endl;
+    }
+};
+
 int main()
 {
 

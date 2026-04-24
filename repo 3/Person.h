132345f558676ea7148b97b7d1df63b1b708b71a
OwaisMazhar-11 <owais74lhr@gmail.com>
2026-04-24 18:34:25 +0500

    Update Person.h

--- repo 3/Person.h
+++ repo 3/Person.h
@@ -1,5 +1,17 @@
 #pragma once
-class person
-{
-
+class Person {
+protected:
+    string name;
+    string email;
+    string passwordHash;
+public:
+    Person(string name, string email, string passwordHash) : name(name), email(email), passwordHash(passwordHash) {}
+    void login() {
+        cout << name << " logged in.\n";
+    }
+    void logout() {
+        cout << name << " logged out.\n";
+    }
+    virtual void authenticate() = 0;  // pure virtual so now Person is abstract
+    virtual ~Person() {}  // virtual destructor cuz its base class
 };

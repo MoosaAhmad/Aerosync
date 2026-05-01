36949458a9acffa127710585ee08e1adaf6a119d
KhurramMushtaq <khurrammushtaq425@gmail.com>
2026-05-02 03:41:56 +0500

    added exception handling and id geberator for receipts repo

--- repo 3/ReceiptRepository.h
+++ repo 3/ReceiptRepository.h
@@ -16,12 +16,24 @@ public:
 	void load() {
 		std::ifstream file(filename);
 		if (!file.is_open())throw std::runtime_error("error opening the file " + filename);
+		
+		int lineNo = 0;
 		std::string buffer;
 		while (getline(file, buffer)) {
+			lineNo++;
 			if (buffer.empty())continue;
-			receipts.push_back(deserialize(buffer));
+			try {
+				receipts.push_back(deserialize(buffer));
+			}
+			catch (const std::runtime_error& e) {
+				throw std::runtime_error(
+					"File: \"" + filename +
+					"\", Line " + std::to_string(lineNo) +
+					": \"" + buffer +
+					"\" corrupted. Reason: " + e.what()
+				);
+			}
 		}
-		file.close();
 	}
 	void save()const{
 		std::ofstream file(filename);
@@ -60,6 +72,19 @@ public:
 		}
 		return nullptr;
 	}
+	//format: RCP-1  RCP-2  RCP-3 ...
+	std::string getNextId() const {
+		int max = 0;
+
+		for (const auto& r : receipts) {
+			std::string id = r->getReceiptId();
+
+			int num = std::stoi(id.substr(4));
+
+			if (num > max)  max = num;
+		}
+		return "RCP-" + std::to_string(max + 1);
+	}
 private:
 	std::string serialize(const Receipt&r) const {
 		std::string line;
@@ -72,7 +97,11 @@ private:
 	}
 	Receipt* deserialize(const std::string line)const {
 		auto slices = Parser::slice(line, '|');
-		if (slices.size() != 3)throw std::runtime_error("invalid format: " + line);
+		if (slices.size() != 3) {
+			throw std::runtime_error("3 fields were expected but got " +
+				std::to_string(slices.size()) +
+				" fields");
+		}
 		return new Receipt(slices[0], slices[1], slices[2]);
 	}
 };

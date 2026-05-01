e88e5096ea2f2d85a363a586a39bd3b88832f480
OwaisMazhar-11 <owais74lhr@gmail.com>
2026-05-02 01:59:23 +0500

    Update Receipt.h

--- repo 3/Receipt.h
+++ repo 3/Receipt.h
@@ -9,17 +9,29 @@ private:
     std::string paymentId;
 
 public:
-    Receipt();
 
+    Receipt() : receiptId(""), bookingId(""), paymentId("") {}
     Receipt(const std::string& rId,
         const std::string& bId,
-        const std::string& pId);
-
-    Receipt(const Receipt& other);
-
-    Receipt& operator=(const Receipt& other);
-
-    std::string getReceiptId() const;
-    std::string getBookingId() const;
-    std::string getPaymentId() const;
+        const std::string& pId)
+        : receiptId(rId), bookingId(bId), paymentId(pId) {
+    }
+    Receipt(const Receipt& other)
+        : receiptId(other.receiptId),
+        bookingId(other.bookingId),
+        paymentId(other.paymentId) {
+    }
+    Receipt& operator=(const Receipt& other)
+    {
+        if (this != &other)
+        {
+            receiptId = other.receiptId;
+            bookingId = other.bookingId;
+            paymentId = other.paymentId;
+        }
+        return *this;
+    }
+    std::string getReceiptId() const { return receiptId; }
+    std::string getBookingId() const { return bookingId; }
+    std::string getPaymentId() const { return paymentId; }
 };

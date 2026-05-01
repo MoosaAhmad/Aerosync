30d993272a8bad79762c94407735d0a11c0819c6
OwaisMazhar-11 <owais74lhr@gmail.com>
2026-05-02 01:59:19 +0500

    Update Booking.h

--- repo 3/Booking.h
+++ repo 3/Booking.h
@@ -13,17 +13,44 @@ protected:
 	datetime bookingDate;
 	BookingStatus status;
 public:
-	Booking(std::string bookingId, std::string userid, std::string flightid, std::string seatNo, datetime bookingTime, BookingStatus stat);
+	Booking(std::string bookingId, std::string userid, std::string flightid, std::string seatNo, datetime bookingTime, BookingStatus stat)
+		:bookingId(bookingId), userId(userid), flightId(flightid), seatNo(seatNo), bookingDate(bookingDate), status(stat) {
+	};
 	
-	std::string getBookingId();
-	BookingStatus getStatus();
-	std::string getFlightId();
-	std::string getUserId();
-	std::string getSeatNumber();
-	datetime    getBookingDate();
+	std::string getBookingId() { return bookingId; }
+	BookingStatus getStatus() { return status; }
+	std::string getFlightId() { return flightId; }
+	std::string getUserId() { return userId; }
+	std::string getSeatNumber() { return seatNo; }
+	datetime    getBookingDate() { return bookingDate; }
 
 
-	/*void confirm();
-	double calcRefund();
-	void cancel();*/
+	/*
+
+//
+//void confirm()
+//{
+//	status = "Confirmed";
+//	seat->reserve();
+//	cout << "Dear " << User->getname();
+//	cout << ",your boking with Id " << bookingId << " is confirmed. \n";
+//	cout << "Seat:" << seat->getSeatNumber() << "reserved" << endl;
+//
+//}
+
+//double calcRefund() {
+//	return status == "Cancelled" ? 0.0 : seat->getPrice();
+//}
+
+//void cancel()
+//{
+//	status = "Cancelled";
+//	seat->release();
+//	cout << "Dear" << User->getname() << " ,your booking with Id" << bookingId;
+//	cout << "is cancelled.\n";
+//	cout << "Refund: " << calcRefund() << endl;
+//}
+
+*/
+
 };

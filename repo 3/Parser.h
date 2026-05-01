364cb59b37a2a9f5540ec96162a2148837c81b5b
Khurram Mushtaq <khurrammushtaq425@gmail.com>
2026-05-01 20:29:48 +0500

    Update Parser.h

--- repo 3/Parser.h
+++ repo 3/Parser.h
@@ -1,17 +1,17 @@
 #pragma once
-//#include"Vector.h"
-#include<string>
-#include<vector>
+#include <string>
+#include <vector>
 
 class Parser {
 public:
-    static vector<string>slice(const string& data, char delimiter) {
-        vector<string>res;
-        if (data.empty())return res;
+    static std::vector<std::string> slice(const std::string& data, char delimiter) {
+        std::vector<std::string> res;
+        if (data.empty()) return res;
 
         int len = data.length();
         int prev = 0;
-        for (int i = 0;i < len;++i) {
+
+        for (int i = 0; i < len; ++i) {
             if (data[i] == delimiter) {
                 res.push_back(data.substr(prev, i - prev));
                 prev = i + 1;
@@ -20,6 +20,7 @@ public:
 
         if (data.back() != delimiter)
             res.push_back(data.substr(prev));
+
         return res;
     }
 };
